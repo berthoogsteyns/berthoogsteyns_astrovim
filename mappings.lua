968384de7e06b1b47bec7a99e58d413297a18944
Bert Hoogsteyns <bhoogsteyns@fastmail.fm>
2023-07-07 15:11:07 +0200

    added easier buffer switch keybind

--- mappings.lua
+++ mappings.lua
@@ -21,6 +21,8 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     ["<leader>z"] = { "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+    ["<S-Tab>"] = { "<cmd>bprev<cr>" },
+    ["<Tab>"] = { "<cmd>bnext<cr>" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
